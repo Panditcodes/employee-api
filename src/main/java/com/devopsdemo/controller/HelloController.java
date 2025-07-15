df4b3aaebcb352c7e2c6424e4c93aa9c379cf636
Panditcodes <148370993+Panditcodes@users.noreply.github.com>
2025-07-16 04:03:42 +0530

    🔁 Trigger Jenkins: updated HelloController

--- src/main/java/com/devopsdemo/controller/HelloController.java
+++ src/main/java/com/devopsdemo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Employee API!";
+        return "Hello from Employee API";
     }
 }
